5a1f4916bd44e7871a5019306c294a13fd4796ce
Dmitrii Gritsai <gritsdmi@fel.cvut.cz>
2019-05-13 19:59:08 +0200

    update constants

--- src/main/java/start/Logic/Constants.java
+++ src/main/java/start/Logic/Constants.java
@@ -21,9 +21,10 @@ public interface Constants {
     int BULLET_SPEED = 10;
     int PLAYER_MOVING_SPEED = 5;
 
-    byte WALL_TYPE_BORDER = 0;
-    byte WALL_TYPE_BRICK = 1;
-    byte WALL_TYPE_TEST = 2;
+    int WALL_TYPE_BORDER = 0;
+    int WALL_TYPE_BRICK = 1;
+    int WALL_TYPE_TEST = 2;
+    int WALL_HEALTH = 50;
 
     int FPS = 30;
 
@@ -33,7 +34,11 @@ public interface Constants {
     int ENEMY_MOVING_SPEED = 2;
     int ENEMY_FIRE_DISTANCE = 300;
     int ENEMY_HEALTH = 80;
-    int ENEMY_SHOOTING_DELAY = 250;
+    int ENEMY_SHOOTING_DELAY = 800;
     int ENEMY_MOVING_OFFSET = 50;
+    int ENEMY_SPAWN_DELAY = 3000;
+    /*this value will multiplied by spawn point count*/
+    int ENEMY_MAX_COUNT_ON_MAP = 3;
+
 
 }
