3cca5ffe34a0ea68dabbbf831dfaf3fe22f2b810
Pavel Pakhomov <pakhopav@fel.cvut.cz>
2019-05-14 21:19:11 +0200

    Internet testing commits

--- src/main/java/start/GameObjects/Bullet.java
+++ src/main/java/start/GameObjects/Bullet.java
@@ -5,7 +5,6 @@ import start.Logic.Constants;
 import start.Logic.SpawnPoint;
 
 import java.awt.*;
-import java.awt.geom.AffineTransform;
 import java.awt.image.AffineTransformOp;
 import java.awt.image.BufferedImage;
 import java.io.Serializable;
@@ -44,7 +43,7 @@ public class Bullet implements Constants, Serializable {
 
         //creating and preparing image to next transformation, scaling if it needed
         scaled = new BufferedImage(imgBuff.getWidth() * (int) (scale), imgBuff.getHeight() * (int) (scale), BufferedImage.TYPE_INT_ARGB);
-
+/*
         AffineTransform at = new AffineTransform();
         at.scale(scale, scale);
         AffineTransformOp scaleOp = new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
@@ -56,6 +55,8 @@ public class Bullet implements Constants, Serializable {
                 Math.toRadians(Math.toDegrees(Math.atan2(direction.y - (this.yPosition), direction.x - (this.xPosition))) + 90),
                 scaled.getWidth() / 2, scaled.getHeight() / 2);
         this.op = new AffineTransformOp(at, AffineTransformOp.TYPE_BILINEAR);
+
+ */
         this.autor = autor;
     }
 
@@ -117,8 +118,10 @@ public class Bullet implements Constants, Serializable {
     public void draw(Graphics2D g) {
 
         if (isAlive) {
-            g.drawImage(op.filter(scaled, null),
-                    (int) (xPosition - scaled.getWidth() / 2), (int) (yPosition - scaled.getHeight() / 2), null);
+//            g.drawImage(op.filter(scaled, null),
+//                    (int) (xPosition - scaled.getWidth() / 2), (int) (yPosition - scaled.getHeight() / 2), null);\
+            g.setColor(Color.BLACK);
+            g.drawOval((int) xPosition, (int) yPosition, 3, 3);
         }
 
     }
